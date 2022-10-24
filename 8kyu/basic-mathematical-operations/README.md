4ae9c8e4674e7e5adb3b9ac3866ae87325855633
Christian Fenn <mythcartoonist@yahoo.com>
2022-10-24 15:12:21 -0400

    Added 'Basic Mathematical Operations' Challenge

---
+++ 8kyu/basic-mathematical-operations/README.md
@@ -0,0 +1,5 @@
+# Basic Mathematical Operations
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
---
+++ 8kyu/basic-mathematical-operations/index.js
@@ -0,0 +1,18 @@
+const basicOp = (operation, value1, value2) => {
+    switch(operation) {
+      case '+':
+        return value1 + value2;
+        break;
+      case '-':
+        return value1 - value2;
+        break;
+      case '*':
+        return value1 * value2;
+        break;
+      case '/':
+        return value1 / value2;
+        break;
+      default:
+        return 0;
+    }
+  }
