6a5b02db92739a3af3b38e7a576067ad4791f67a
Christian Fenn <mythcartoonist@yahoo.com>
2022-11-15 11:45:54 -0500

    Added 'Reverse Letter' Challenge

---
+++ 7kyu/reverse-letter/README.md
@@ -0,0 +1,2 @@
+# Reverse Letter
+Given a string ```str```, reverse it and omit all non-alphabetic characters.
---
+++ 7kyu/reverse-letter/index.js
@@ -0,0 +1,1 @@
+const reverseLetter = str => str.split('').filter(c => c.match(/[a-zA-Z]/)).reverse().join('')
